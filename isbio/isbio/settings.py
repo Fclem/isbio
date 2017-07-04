b957f9b677956455f3ad674e2264dfa4642707b9
Clément Fiere <clement.fiere@helsinki.fi>
2017-07-04 17:03:25 +0300

    changed dev domain, and IPS

--- isbio/isbio/settings.py
+++ isbio/isbio/settings.py
@@ -285,8 +285,8 @@ LOGGING = {
 
 
 class DomainList(object):
-	CLOUD_PROD = ['breeze.fimm.fi', '52.164.211.188', ]
-	CLOUD_DEV = ['breeze-dev.cloudapp.net', '40.113.91.111', ]
+	CLOUD_PROD = ['breeze.fimm.fi', '52.169.249.211', ]
+	CLOUD_DEV = ['breeze-dev.northeurope.cloudapp.azure.com', '52.164.209.61', ]
 	FIMM_PH = ['breeze-newph.fimm.fi', 'breeze-ph.fimm.fi', ]
 	FIMM_DEV = ['breeze-dev.fimm.fi', ]
 	FIMM_PROD = ['breeze-fimm.fimm.fi', 'breeze-new.fimm.fi', ]
