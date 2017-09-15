4441d6011803733d9aaa18d96537648c1da38b04
Clément Fiere <clement.fiere@helsinki.fi>
2017-09-15 16:01:35 +0300

    improved termcoloring class

--- isbio/utilz/system.py
+++ isbio/utilz/system.py
@@ -14,6 +14,7 @@ __date__ = '27/05/2016'
 class TermColoring(enumerate):
 	HEADER = '\033[95m'
 	OK_BLUE = '\033[94m'
+	DARK_BLUE = '\033[34m'
 	OK_GREEN = '\033[92m'
 	WARNING = '\033[93m'
 	FAIL = '\033[91m'
@@ -22,32 +23,151 @@ class TermColoring(enumerate):
 	UNDERLINE = '\033[4m'
 
 	@classmethod
-	def ok_blue(cls, text):
-		return cls.OK_BLUE + text + cls.END_C
+	def ok_blue(cls, text, print_func=None):
+		"""  blue coloring
+
+		:param text: a string to be colored in blue
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__text_gen(text, cls.OK_BLUE, print_func)
 
 	@classmethod
-	def ok_green(cls, text):
-		return cls.OK_GREEN + text + cls.END_C
+	def ok_green(cls, text, print_func=None):
+		"""  green coloring
+
+		:param text: a string to be colored in green
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__text_gen(text, cls.OK_GREEN, print_func)
 
 	@classmethod
-	def fail(cls, text):
-		return cls.FAIL + text + cls.END_C
+	def fail(cls, text, print_func=None):
+		"""  red coloring
+
+		:param text: a string to be colored in red
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__text_gen(text, cls.FAIL, print_func)
 
 	@classmethod
-	def warning(cls, text):
-		return cls.WARNING + text + cls.END_C
+	def warning(cls, text, print_func=None):
+		"""  Yellow coloring
+
+		:param text: a string to be colored in yellow
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__text_gen(text, cls.WARNING, print_func)
 
 	@classmethod
-	def header(cls, text):
-		return cls.HEADER + text + cls.END_C
+	def header(cls, text, print_func=None):
+		"""  ?
+
+		:param text: a string to be ?
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__text_gen(text, cls.HEADER, print_func)
 
 	@classmethod
-	def bold(cls, text):
-		return cls.BOLD + text + cls.END_C
+	def bold(cls, text, print_func=None):
+		"""  font weight bold
+
+		:param text: a string to put into bold
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__text_gen(text, cls.BOLD, print_func)
 
 	@classmethod
-	def underlined(cls, text):
-		return cls.UNDERLINE + text + cls.END_C
+	def underlined(cls, text, print_func=None):
+		"""  underlining of text
+
+		:param text: a string to be underlined
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__text_gen(text, cls.UNDERLINE, print_func)
+	
+	# clem 15/09/2017
+	@classmethod
+	def dark_blue(cls, text, print_func=None):
+		"""  dark blue coloring
+		
+		:param text: a string to be colored in dark blue
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__text_gen(text, cls.DARK_BLUE, print_func)
+	
+	# clem 15/09/2017
+	@classmethod
+	def no_color(cls, text, print_func=None):
+		""" no coloring
+		
+		:param text: a string not to be colored
+		:type text: basestring
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__printer_router(text, print_func)
+	
+	# clem 15/09/2017
+	@classmethod
+	def __text_gen(cls, text, head, print_func):
+		""" writing shortcut that use head color string adds text and the trailing ENC_C
+		
+		:param text: a string to be colored
+		:type text: basestring
+		:param head: the color code to use
+		:type head: str
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		return cls.__printer_router(head + text + cls.END_C, print_func)
+	
+	# clem 15/09/2017
+	@classmethod
+	def __printer_router(cls, value, print_func=None):
+		""" return value or return print_func(value)
+		
+		:type value: basestring
+		:type print_func: function
+		:rtype: basestring | (print_func)
+		"""
+		if print_func:
+			return print_func(value)
+		else:
+			return value
+	
+	# clem 15/09/2017
+	@classmethod
+	def command(cls, command, color=None, print_func=None):
+		""" format a string like a shell input (add a leading green $)
+		
+		:param command: a string to be colored as a shell command
+		:type command: basestring
+		:param color: an optional coloring function for the command line (defaults to DEFAULT_COLOR)
+		:type color: function
+		:param print_func: an optional output function (like print())
+		:type print_func: function
+		"""
+		color = cls.DEFAULT_COLOR if not color else color
+		value = cls.ok_green('$ ') + color(command)
+		return cls.__printer_router(value, print_func)
+	
+	DEFAULT_COLOR = dark_blue
 
 
 # clem 19/02/2016
