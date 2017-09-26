a151032ed93fd0f387122a694812046bf4d13f14
Clément Fiere <clement.fiere@helsinki.fi>
2017-09-26 15:57:35 +0300

    reverting bad changes

--- isbio/custom_auth0/settings.py
+++ isbio/custom_auth0/settings.py
@@ -5,11 +5,12 @@ from utilz import get_key
 
 
 CALLBACK_NAME = 'AUTH0_CALLBACK_URL'
-# AUTH0_CALLBACK = reverse('django.auth0.views.auth_callback')
 AUTH0_CALLBACK = reverse('custom_auth0.views.auth_callback')
 AUTH0_CALLBACK_URL = getattr(settings, CALLBACK_NAME, AUTH0_CALLBACK)
 
-AUTH0_IP_LIST = ['52.169.124.164', '52.164.211.188', '52.28.56.226', '52.28.45.240', '52.16.224.164', '52.16.193.66']
+AUTH0_IP_LIST = ['34.253.4.94', '35.156.51.163', '35.157.221.52', '52.16.193.66', '52.16.224.164',
+	'52.28.45.240', '52.28.56.226', '52.28.184.187', '52.28.212.16', '52.29.176.99', '52.50.106.250',
+	'52.57.230.214', '52.211.56.181', '52.213.216.142', '52.213.38.246', '52.213.74.69']
 AUTH0_DOMAIN = 'breeze.eu.auth0.com'
 AUTH0_TEST_URL = 'https://%s/test' % AUTH0_DOMAIN
 AUTH0_ID_FILE_N = 'auth0_id'
@@ -17,7 +18,6 @@ AUTH0_CLIENT_ID = get_key(AUTH0_ID_FILE_N)
 AUTH0_SECRET_FILE_N = 'auth0'
 AUTH0_SECRET = get_key(AUTH0_SECRET_FILE_N)
 AUTH0_CALLBACK_URL_BASE = 'https://%s/login/'
-# AUTH0_CALLBACK_URL = 'https://breeze.fimm.fi/login/'
 AUTH0_SUCCESS_URL = '/home/'
 AUTH0_LOGOUT_URL = 'https://breeze.eu.auth0.com/v2/logout?returnTo=%s'
 AUTH0_OAUTH_TOKEN_URL = 'https://%s/oauth/token' % AUTH0_DOMAIN
