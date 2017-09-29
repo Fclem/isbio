8457f3b596a9fc4c376860e57e98c295dc379631
Clément Fiere <clement.fiere@helsinki.fi>
2017-09-29 19:39:09 +0300

    debug1

--- isbio/isbio/config/mode/__init__.py
+++ isbio/isbio/config/mode/__init__.py
@@ -1,4 +1,5 @@
-from .. import GENERATED_MODULE_NAME, conf_gen, logging
+from .. import GENERATED_MODULE_NAME, conf_gen
+from utilz import logger
 import os
 
 __path__ = os.path.realpath(__file__)
@@ -9,7 +10,7 @@ conf_gen(__dir_path__, 'ConfigRunModesList')
 try:
 	from __generated import config_list
 except (SyntaxError, ImportError, Exception) as e:
-	logging.error('importing __generated: %s' % e)
+	logger.error('importing __generated: %s' % e)
 	import importlib
 	
 	generated = importlib.import_module(GENERATED_MODULE_NAME, package='isbio.config.mode')
