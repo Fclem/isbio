58d62e6bbba3496de728aae9e5dd9dfdae07609b
Clément Fiere <clement.fiere@helsinki.fi>
2017-09-29 19:30:44 +0300

    new env conf file for breeze-web (cloud breeze at FIMM), backport of new auth0 ips, added FIMM_CLOUD to DomainList

--- isbio/isbio/config/env/FIMM_cloud.py
+++ isbio/isbio/config/env/FIMM_cloud.py
@@ -1,7 +1,7 @@
 from isbio.config.execution.docker import * # !important, do not delete
 from isbio.settings import SOURCE_ROOT, DomainList
 from isbio.config import DEV_MODE, BREEZE_PROD_FOLDER
-DOMAIN = DomainList.CLOUD_DEV if DEV_MODE else DomainList.CLOUD_PROD
+DOMAIN = DomainList.FIMM_CLOUD
 from auth.auth0 import *
 
 ALLOWED_HOSTS = DOMAIN + AUTH0_IP_LIST
@@ -13,12 +13,12 @@ BREEZE_FOLDER = '%s/' % BREEZE_PROD_FOLDER
 
 # might go into docker config ?
 DOCKER_HUB_PASS_FILE = SOURCE_ROOT + 'docker_repo'
-AZURE_PASS_FILE = SOURCE_ROOT + 'azure_pwd'
+AZURE_PASS_FILE = SOURCE_ROOT + 'azure_pwd' # TODO check
 # override Shiny mode (may be on if run mode is dev)
 SHINY_MODE = 'remote'
 SHINY_LOCAL_ENABLE = False
-BREEZE_TITLE = 'C-BREEZE' + ('-DEV' if DEV_MODE else '')
-BREEZE_TITLE_LONG = 'Cloud Breeze' + (' (dev)' if DEV_MODE else '')
+BREEZE_TITLE = 'C-BREEZE2' + ('-DEV' if DEV_MODE else '')
+BREEZE_TITLE_LONG = 'Cloud Breeze2' + (' (dev)' if DEV_MODE else '')
 
 STATICFILES_DIRS = (
 	# Put strings here, like "/home/html/static" or "C:/www/django/static".
